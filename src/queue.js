9186ac837357a7b4a7020b305991b226c507fcfc
Dave <wm.david.gallup@gmail.com>
2017-06-12 13:36:20 -0700

    Complete refactor

--- src/queue.js
+++ src/queue.js
@@ -8,16 +8,26 @@ class Queue{
   }
 
   size(){
-    
+    return this._stack_L.size();
   }
 
   enqueue(el){
-
-
+    this._stack_L.push(el);
   }
 
   dequeue(){
+    var result;
 
+    //pop all from L and push to R
+    while(this._stack_L.size() > 0){
+      this._stack_R.push(this._stack_L.pop());
+    }
+    result = this._stack_R.pop();
+
+    while(this._stack_R.size() > 0){
+      this._stack_L.push(this._stack_R.pop());
+    }
+    return result;
   }
 }
 
